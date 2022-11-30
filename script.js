787e1e6b29faafe7cb5b62d08bfc6a2b4212fad2
EleneMtchedlishvili <112971990+EleneMtchedlishvili@users.noreply.github.com>
2022-12-01 02:13:16 +0400

    show results (comics)

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-getCharacters();
+const characters =
+  "https://gateway.marvel.com/v1/public/characters?apikey=97d62f598e1023a90303368f09b1004a";
+const comics =
+  "https://gateway.marvel.com/v1/public/comics?apikey=97d62f598e1023a90303368f09b1004a";
 
-async function getCharacters() {
+// getCharacters();
+
+// async function getCharacters() {
+//   const res = await fetch(
+//     "https://gateway.marvel.com/v1/public/characters?apikey=97d62f598e1023a90303368f09b1004a"
+//   );
+
+//   console.log(await res.json());
+// }
+
+getComics();
+
+async function getComics() {
   const res = await fetch(
-    "https://gateway.marvel.com/v1/public/characters?apikey=97d62f598e1023a90303368f09b1004a"
+    "https://gateway.marvel.com/v1/public/comics?apikey=97d62f598e1023a90303368f09b1004a"
   );
 
   console.log(await res.json());
