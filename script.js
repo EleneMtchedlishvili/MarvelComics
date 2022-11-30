394687913885a85ce8ea37d9e27007f50ccee01b
EleneMtchedlishvili <112971990+EleneMtchedlishvili@users.noreply.github.com>
2022-12-01 01:13:20 +0400

    test request

---
+++ script.js
@@ -0,0 +1,9 @@
+getCharacters();
+
+async function getCharacters() {
+  const res = await fetch(
+    "https://gateway.marvel.com/v1/public/characters?apikey=97d62f598e1023a90303368f09b1004a"
+  );
+
+  console.log(res);
+}
