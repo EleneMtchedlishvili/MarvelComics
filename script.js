34ccc43efed572ab39223e72091fcc33c05f36ee
EleneMtchedlishvili <112971990+EleneMtchedlishvili@users.noreply.github.com>
2022-12-01 01:28:13 +0400

    add await

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ async function getCharacters() {
     "https://gateway.marvel.com/v1/public/characters?apikey=97d62f598e1023a90303368f09b1004a"
   );
 
-  console.log(res.json());
+  console.log(await res.json());
 }
